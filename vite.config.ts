67a51f8657ea46097e99f81093a82756ce6a5c65
ahlawatprashantchaudhary-lgtm <ahlawatprashantchaudhary@gmail.com>
2026-09-15 19:16:24 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Agri-Concept-andFact10/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
